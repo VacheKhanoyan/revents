19acc39240bab8461bd469674b5404eab2dacd10
VacheKhanoyan <khanoyann@gmail.com>
2018-07-08 13:20:09 +0400

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        re-vents
+      </div>
+    );
+  }
+}
+
+export default App;
