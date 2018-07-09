a4a51f3836e3b8bac15129c7668d0fc5e00d1818
VacheKhanoyan <khanoyann@gmail.com>
2018-07-09 16:38:05 +0400

    createEvent

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,39 +1,87 @@
-import React, { Component } from 'react'
-import { Segment,Form, Button, } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
 
- class EventForm extends Component {
+class EventForm extends Component {
+  state = {
+    event: {
+      title: "",
+      date: "",
+      city: "",
+      venue: "",
+      hostedBy: ""
+    }
+  };
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.createEvent(this.state.event);
+  };
+  onInputChange = e => {
+    const newEvent = this.state.event;
+    newEvent[e.target.name] = e.target.value;
+    this.setState({ event: newEvent });
+  };
   render() {
+    const { handleCancel } = this.props;
+    const { event } = this.state;
     return (
-           <Segment>
-             <Form>
-               <Form.Field>
-                 <label>Event Title</label>
-                 <input placeholder="First Name" />
-               </Form.Field>
-               <Form.Field>
-                 <label>Event Date</label>
-                 <input type="date" placeholder="Event Date" />
-               </Form.Field>
-               <Form.Field>
-                 <label>City</label>
-                 <input placeholder="City event is taking place" />
-               </Form.Field>
-               <Form.Field>
-                 <label>Venue</label>
-                 <input placeholder="Enter the Venue of the event" />
-               </Form.Field>
-               <Form.Field>
-                 <label>Hosted By</label>
-                 <input placeholder="Enter the name of person hosting" />
-               </Form.Field>
-               <Button positive type="submit">
-                 Submit
-               </Button>
-               <Button type="button">Cancel</Button>
-             </Form>
-           </Segment>
-    )
+      <Segment>
+        <Form onSubmit={this.onFormSubmit}>
+          <Form.Field>
+            <label>Event Title</label>
+            <input
+              name="title"
+              onChange={this.onInputChange}
+              value={event.title}
+              placeholder="Event Title"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Event Date</label>
+            <input
+              type="date"
+              name="date"
+              onChange={this.onInputChange}
+              value={event.date}
+              placeholder="Event Title"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>City</label>
+            <input
+              name="city"
+              onChange={this.onInputChange}
+              value={event.city}
+              placeholder="Event Title"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Venue</label>
+            <input
+              name="venue"
+              onChange={this.onInputChange}
+              value={event.venue}
+              placeholder="Event Title"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Hosted By</label>
+            <input
+              name="hostedBy"
+              onChange={this.onInputChange}
+              value={event.hostedBy}
+              placeholder="Event Title"
+            />
+          </Form.Field>
+          <Button positive type="submit">
+            Submit
+          </Button>
+          <Button type="button" onClick={handleCancel}>
+            Cancel
+          </Button>
+        </Form>
+      </Segment>
+    );
   }
 }
 
-export default EventForm
+export default EventForm;
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import { Segment,Item, Icon, List, Button } from 'semantic-ui-react'
-import EventListAttendee from './EventListAttendee';
+import React, { Component } from "react";
+import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
+import EventListAttendee from "./EventListAttendee";
 
- class EventListItem extends Component {
+class EventListItem extends Component {
   render() {
-    const {event}=this.props
+    const { event } = this.props;
     return (
-     <Segment.Group>
+      <Segment.Group>
         <Segment>
           <Item.Group>
             <Item>
-              <Item.Image size="tiny" circular src={event.hostPhotoURL}/>
+              <Item.Image size="tiny" circular src={event.hostPhotoURL} />
               <Item.Content>
                 <Item.Header as="a">{event.title}</Item.Header>
                 <Item.Description>
@@ -28,19 +28,19 @@ import EventListAttendee from './EventListAttendee';
         </Segment>
         <Segment secondary>
           <List horizontal>
-{event.attendees.map(attendee=>(
-  <EventListAttendee key={attendee.id} attendee={attendee}/>       
-))
-               }
+            {event.attendees &&
+              event.attendees.map(attendee => (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
+              ))}
           </List>
         </Segment>
         <Segment clearing>
-        <span>{event.description}</span>
+          <span>{event.description}</span>
           <Button as="a" color="teal" floated="right" content="View" />
         </Segment>
       </Segment.Group>
-    )
+    );
   }
 }
 
-export default EventListItem
+export default EventListItem;
